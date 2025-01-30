b2ce34b87c0be3d231358230b4c835f0c2d58ab1
mzerhusen <zerhusma@mail.uc.edu>
2025-01-29 22:54:36 -0500

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int intOperandA = 2;
+        int intOperandB = 33;
+        int intSum = 342;
+        int intProduct = 393;
+        int intDifference = 94;
+        int intQuotient = 102;
+        int intModulo = 100020;
+        double doubleOperandA = 9.3;
+        double doubleOperandB = 5.5;
+        double doubleSum = 32.3;
+        double doubleProduct = 9210.2;
+        double doubleDifference = 512.32;
+        double doubleQuotient = 1.11111;
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum using ints of " + intOperandA + " and " + intOperandB + " is " + intSum);
     }
 }
